e6cca218675a7cbe774f38a7302dc869db69e67e
akshathgk012 <akshathagk012@gmail.com>
2026-03-01 07:12:33 +0000

    Made changes in settings.py

--- lms_main/settings.py
+++ lms_main/settings.py
@@ -217,8 +217,8 @@ TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
 TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
 TWILIO_PHONE_NUMBER = os.getenv("TWILIO_PHONE_NUMBER")
 
-<<<<<<< HEAD
-=======
+
+
 EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
 
 EMAIL_HOST = os.getenv("EMAIL_HOST")
@@ -227,4 +227,4 @@ EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "True"
 
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
->>>>>>> b4f099b (added lead_mail)
+
