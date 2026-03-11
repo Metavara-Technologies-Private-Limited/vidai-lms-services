2a0935065d3d4d33edec95a8e882b811a98290fe
saimohan <saimohan6056@gmail.com>
2026-03-11 15:34:10 +0530

    Moved database credentials to env

--- lms_main/settings.py
+++ lms_main/settings.py
@@ -97,11 +97,11 @@ WSGI_APPLICATION = 'lms_main.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'stage5_db',
-        'USER': 'postgres',
-        'PASSWORD': 'saimohan',
-        'HOST': 'host.docker.internal', # ✅ UPDATED (important for Docker) host.docker.internal
-        'PORT': '5432',   # ✅ ADDED (important)
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
     }
 }
 
