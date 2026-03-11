e4c8058a7f3f883a037e6daf9beb86977de82dd4
saimohan <saimohan6056@gmail.com>
2026-03-11 16:34:29 +0530

    added db

--- lms_main/settings.py
+++ lms_main/settings.py
@@ -97,11 +97,11 @@ WSGI_APPLICATION = 'lms_main.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
+        'NAME': 'stage5_db',
+        'USER': 'postgres',
+        'PASSWORD': 'saimohan',
+        'HOST': 'host.docker.internal',#  UPDATED for Docker compatibility host.docker.internal
+        'PORT': '5432',
     }
 }
 
