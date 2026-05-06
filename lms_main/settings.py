2c9f15d02142b05a054e7fdbe34a1f4940c883b2
GORRELA SAI MOHAN <saimohan6056@gmail.com>
2026-05-06 10:52:24 +0530

    settings.py

--- lms_main/settings.py
+++ lms_main/settings.py
@@ -111,7 +111,7 @@ WSGI_APPLICATION = 'lms_main.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'stage6_db',
+        'NAME': 'stage5_db',
         'USER': 'postgres',
         'PASSWORD': 'saimohan',
         'HOST': 'host.docker.internal',  # 'host.docker.internal',   #host.docker.internal
